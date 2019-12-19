3d387e700babb284682a70f4c3b9dd506f6587c9
蔡凯歌 <caikaige.g@gmail.com>
2019-12-19 13:33:02 +0800

    init

---
+++ ios/Classes/TencentAdPlugin.m
@@ -0,0 +1,15 @@
+#import "TencentAdPlugin.h"
+#if __has_include(<tencent_ad/tencent_ad-Swift.h>)
+#import <tencent_ad/tencent_ad-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "tencent_ad-Swift.h"
+#endif
+
+@implementation TencentAdPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftTencentAdPlugin registerWithRegistrar:registrar];
+}
+@end
